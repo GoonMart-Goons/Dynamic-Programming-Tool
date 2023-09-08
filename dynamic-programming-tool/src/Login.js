d5261b02f5eab90e072399ab1e977f4584a5024a
Milisa Sithole <127535133+MilisaSithole@users.noreply.github.com>
2023-09-08 21:00:18 +0200

    Login validation done

--- dynamic-programming-tool/src/Login.js
+++ dynamic-programming-tool/src/Login.js
@@ -1,12 +1,44 @@
-import React from "react";
-import { Form, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Styles/Login.css";
 import dpLogo from './Images/dp2.png';
 import { useNavigate } from "react-router-dom";
 
-function Login() {
+//For form validation
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
+
+function Login() {
     const navigate = useNavigate();
+    
+    //For validation
+    const validationSchema = Yup.object().shape({
+        studentNumber: Yup.string()
+            .required('Student number is required')
+            .matches(/^[0-9]+$/, 'Student number must consist of numbers only.')
+            .length(7, 'Student number must be 7 digits.'),
+        password: Yup.string()
+            .required('Password is required')
+            .matches(/^\d{8}$/, 'Password number must be at least 8 characters.'),
+    })
+
+    const handleSubmit = (e) => {
+        const errors = Object.keys(e).reduce((acc, fieldName) => {
+            try{
+                validationSchema.validateSyncAt(fieldName, e)
+                return acc
+            } catch (error) {
+                return { ...acc, [fieldName]: error.message }
+            }
+        }, {})
+        
+        console.log("Errors:", errors)
+
+        if(Object.keys(errors).length === 0){
+            navigate('/home')
+        }
+    }
 
     return (
         <div className = "login">
@@ -16,7 +48,29 @@ function Login() {
 
             <div className = "login-container">
                 <h1>Login to DP World</h1>
-                <form onSubmit = {() => navigate('/home')}>
+                <Formik
+                    initialValues={{ studentNumber: "", password: "" }}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    <Form>
+                        <label htmlFor="studentNumber">Student Number</label>
+                        <div>
+                            <Field type="text" id="studentNumber" className="login-field" name="studentNumber"/>
+                            <ErrorMessage name="studentNumber" className="login-errMsg" component="div"/>
+                        </div>
+
+                        <label htmlFor="password">Password</label>
+                        <div>
+                            <Field type="password" className="login-field" name="password" />
+                            <ErrorMessage name="password" className="login-errMsg" component="div"/>
+                        </div>
+                        
+                        <button type = "submit" className = "login-button">LOGIN</button>
+                    </Form>
+                </Formik>
+
+                {/* <form onSubmit = {handleSubmit}>
                     <h5>STUDENT NUMBER</h5>
                     <input type = "text" placeholder="Enter your student number" />
 
@@ -25,7 +79,7 @@ function Login() {
 
                     <button type = "submit" className = "login-button">LOGIN</button>
 
-                </form>
+                </form> */}
 
                 <p> By logging in you agree to DP World's Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.</p>
                 {/* <Link to = "/login" className = "login-link">
