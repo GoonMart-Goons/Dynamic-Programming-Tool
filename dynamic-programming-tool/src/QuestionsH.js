fe858ef7d4984510acad3ab1e3c87263fb53a523
Milisa Sithole <127535133+MilisaSithole@users.noreply.github.com>
2023-10-09 22:33:24 +0200

    Questions page working

--- dynamic-programming-tool/src/QuestionsH.js
+++ dynamic-programming-tool/src/QuestionsH.js
@@ -6,7 +6,13 @@ import profilePic from './Images/profile.png';
 import "./Styles/TopDown.css";
 import Navbar from "./Navbar";
 
+//Can Sum Question test
+import { getQuestion, getAnswer } from "./Algos/canSum";
+
 function QuestionsH(){
+    const questionText = getQuestion()
+    const serializedTree = getAnswer()
+
     return(
         <div className="top-down">
             <Navbar placeholder=""></Navbar>
@@ -14,8 +20,8 @@ function QuestionsH(){
             <div className="top-down-container">
                 <div className="top-down-side-container">
                 <h1>Question</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porttitor pulvinar est, vitae fringilla orci euismod vel. In convallis finibus rutrum. Nam et ante id velit ultrices faucibus sit amet sed odio. Duis dignissim pellentesque justo, nec interdum odio tempor in. Nulla pellentesque lectus id facilisis sollicitudin. Quisque mattis purus quis condimentum lobortis. Fusce fringilla molestie metus vitae commodo. Maecenas tincidunt aliquam semper. In vehicula nulla nibh, nec blandit mi dignissim in.</p>
-                    
+                <p>{questionText}</p>
+
                 </div>
                 <div className="top-down-main-container">
                     <h1>Answer</h1>
