8c5a5aa548dc9bd6da99af6a520e9eb4690eabf4
MilagroTheBuoy <milagrodakid@gmail.com>
2023-11-15 20:52:33 +0200

    Added comments and removed redundant code

--- dynamic-programming-tool/src/Components/graph.js
+++ dynamic-programming-tool/src/Components/graph.js
@@ -1,3 +1,7 @@
+/*A large portion of the code seen in the GraphView function was inspired by code
+found here: https://visjs.github.io/vis-network/docs/network/ and
+here: https://www.npmjs.com/package/react-vis-graph-wrapper */
+
 import React, { useState } from "react";
 import VisGraph, {
     GraphData,
@@ -5,12 +9,10 @@ import VisGraph, {
     Options,
   } from 'react-vis-graph-wrapper';
 import "../Styles/graph.css";
-import { Formik, Field, Form, ErrorMessage, setIn } from 'formik';
-import * as Yup from 'yup';
 import 'reactjs-popup/dist/index.css';
 import { EditText, EditTextarea } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
-//import "../Styles/Popup.css"
+
 //Used to export the user's answer
 import { Tree, TreeNode } from "../Classes/TreeClass";
 import WrittenAnswer from "./writtenAnswer";
@@ -85,78 +87,11 @@ function getValueFromLabel(label){
 
 function GraphView({questionNumber}){
 
-    //const vis = document.getElementById("myVisGraph");
-
-    const [namePopupComponent, setNamePopupComponent] = useState(false);
     const [namePopupEditText, setNamePopupEditText] = useState("");
-    const [nodeContainer, setNodes] = useState([]);
-    const [edgeContainer, setEdges] = useState([]);
-
-    const editValidationSchema = Yup.object().shape({
-        nodeToEdit: Yup.number()
-            .required('You must enter the ID of a node to edit.')
-            .typeError('Node ID must be a number'),
-        newLabel: Yup.number()
-            .required('You must enter a label.')
-            .typeError('Node label must be a number')
-    });
-
-    const addValidationSchema = Yup.object().shape({
-        nodeToAdd: Yup.number()
-            .required('Node must have a label.')
-            .typeError('Node label must be a number'),
-        edgeToAdd: Yup.number()
-            .required('Node must have parent.')
-            .typeError('Node parent must have a label that is a number'),
-    });
 
     const createLabel = (identity, label) => {
-        //await setNamePopupComponent(true);
         return "id: " + identity + "\nlabel: " + label;;
     }
-
-    const createNode = (e) => {
-        identity += 1;
-        var newLabel = createLabel(identity, e.nodeToAdd);
-        const newNode = {
-          id: identity,
-          label: newLabel,
-        };
-        return newNode;
-    }
-
-    /*const addNode = (e) => {
-        var foundObject;
-        var newEdge;
-        var newNode;
-        foundObject = nodeContainer.find((item) => item.id == e.edgeToAdd);
-
-        //only creates node if the parent ID exists or if the graph is empty
-        if(foundObject != null){
-            newNode = createNode(e);
-            newEdge = {
-                from: foundObject.id,
-                to: newNode.id,
-            };
-        }
-        else{
-            if(nodeContainer.length === 0){
-                newNode = createNode(e);
-                newEdge = {
-                    from: newNode.id,
-                    to: newNode.id,
-                };
-            }
-            else{
-                alert("Node not in the tree.");
-                return;
-            }
-            
-        }
-        setNodes([...nodeContainer, newNode]);
-        setEdges([...edgeContainer, newEdge]);
-        console.log(nodeContainer);
-    };*/
 
     const graph = {
         nodes: nodeArray,
@@ -165,12 +100,6 @@ function GraphView({questionNumber}){
     }
     
     var option = {
-        /*configure: {
-            enabled: true,
-            filter: 'nodes,edges',
-            container: undefined,
-            showButton: true
-        },*/
         edges: {
             color:"black",
             arrows:{
@@ -188,41 +117,31 @@ function GraphView({questionNumber}){
         interaction: {
             dragNodes:true,
         },
-
-        /*layout: {
-            randomSeed: undefined,
-            improvedLayout:true,
-            clusterThreshold: 150,
-            hierarchical: {
-              enabled:true,
-              levelSeparation: 100,
-              nodeSpacing: 100,
-              treeSpacing: 200,
-              blockShifting: true,
-              edgeMinimization: true,
-              parentCentralization: true,
-              direction: 'LR',        // UD, DU, LR, RL
-              sortMethod: 'hubsize',  // hubsize, directed
-              shakeTowards: 'roots'  // roots, leaves
-            }
-        },*/
-
         manipulation: {
+
+            //addNode, addEdge, editNode and deleteNode are custom written functions
             addNode: function(nodeData,callback) {
                 identity++;
+
+                //sets node colour to green for the root
                 if(nodeArray.length === 0){
                     nodeData.color = "#00ff00"
                     identity = 0
                 }
                 nodeData.id = identity;
                 nodeData.label = createLabel(identity, namePopupEditText);
-                nodeArray.push(nodeData)
+                nodeArray.push(nodeData);
+
+                //adds nodes to the display
                 callback(nodeData);
             },
+
             addEdge: function(edgeData,callback) {
+
+                //checks to see if user dragged the edge to node of origin
                 if (edgeData.from === edgeData.to) {
-                  var r = window.confirm("Do you want to connect the node to itself?");
-                  if (r === true) {
+                  var wantsSelfConnection = window.confirm("Do you want to connect the node to itself?");
+                  if (wantsSelfConnection === true) {
                     edgeArray.push(edgeData)
                     callback(edgeData);
                   }
@@ -232,28 +151,35 @@ function GraphView({questionNumber}){
                     callback(edgeData);
                 }
             },
+
             editNode: function(nodeData,callback) {
                 var currID = nodeData.id
                 var newLabel = createLabel(currID, namePopupEditText);
                 nodeData.label = newLabel;
+
+                //edits node information in the list of nodes
                 var foundNode = nodeArray.findIndex((item) => item.id == currID);
                 nodeArray[foundNode].label = newLabel;
+
+                //edits node on display
                 callback(nodeData);
             },
-            editEdge: true,
+
+            editEdge: false,
             deleteNode: function(nodeData, callback){
-                var foundNode, foundEdge;
-                foundNode = nodeArray.findIndex((item) => item.id == nodeData.nodes[0]);
-                foundEdge = edgeArray.findIndex((item) => item.from == nodeData.nodes[0]);
                 if(nodeData.edges.length != 0){
+                    
                     var nodeID = nodeData.nodes[0];
                     var fromExists = false;
-                    console.log(edgeArray.length)
+
+                    //looks through the edgeArray to check whether any edge indicates that a node is a parent
                     for(var i = 0; i < edgeArray.length; i++){
                         if(nodeID === edgeArray[i].from){
                             fromExists= true;
                         }
                     }
+
+                    //allows deletion only if node is not a parent
                     if(!fromExists){
                         callback(nodeData);
                         nodeArray = nodeArray.filter(obj => obj.id !== nodeData.nodes[0]);
@@ -272,10 +198,10 @@ function GraphView({questionNumber}){
             deleteEdge: true
         },
 
+        //when enabled, nodes will bounce away from each other we=hen moved closer together
         physics: {
             enabled: false
         }
-        /**/
     }
 
     const handleAdd = (e, setFn) => {
