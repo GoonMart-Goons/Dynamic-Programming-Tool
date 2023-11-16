f3bf689a88746428b6f516e4a973235ea0a51bdd
kudzaiSaurombe <82084840+kudzaiSaurombe@users.noreply.github.com>
2023-11-16 17:56:25 +0200

    update login test and questionsh test

--- dynamic-programming-tool/src/Tests/Login.test.js
+++ dynamic-programming-tool/src/Tests/Login.test.js
@@ -4,6 +4,7 @@ import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom"; // Import the library itself 
 
 import Login from "../Login";
+import Register from "../Register";
 
 //DISPLAY
 test("renders Logo", () => {
@@ -79,7 +80,7 @@ test("renders Button", () => {
   });
 
   //ERRORS
-  test("User can enter text in email and password fields", () => {
+  test("User can enter text in email and password fields", async() => {
     const { getByPlaceholderText, getByText } = render(
       <MemoryRouter>
         <Login />
@@ -94,11 +95,61 @@ test("renders Button", () => {
 
     fireEvent.blur(passwordInput);
 
-    const passwordError = getByText("Password must be at least 8 characters.");
-  
-    // expect(passwordError).toBeInTheDocument();
 
-    // await waitFor(() => {
-        expect(passwordError).toBeInTheDocument();
-    // });
+    await waitFor(() => {
+        expect(screen.getByText('Password must be at least 8 characters.')).toBeInTheDocument();
+    });
+});
+
+
+test('displays error message for invalid email', async () => {
+    render(
+        <MemoryRouter>
+            <Login />
+        </MemoryRouter>
+    );
+    fireEvent.change(screen.getByPlaceholderText('Enter your email address'), {
+        target: { value: 'invalidemail' }, // Invalid first name
+    });
+    fireEvent.blur(screen.getByPlaceholderText('Enter your email address')); // to trigger validation
+
+    // Wait for the error message to appear
+    await waitFor(() => {
+        expect(screen.getByText('Invalid email format.')).toBeInTheDocument();
+    });
+});
+
+test("submits form with valid data", async () => {
+
+    const { getByPlaceholderText, getByText } = render(
+        <MemoryRouter>
+            <Login />
+        </MemoryRouter>
+    );
+
+    fireEvent.change(getByPlaceholderText("Enter your email address"), { target: { value: "test@example.com" } });
+    fireEvent.change(getByPlaceholderText("Enter your password"), { target: { value: "password123" } });
+    fireEvent.click(getByText("LOGIN"));
+
+    await waitFor(() => {
+        //expect(mockSignIn).toHaveBeenCalledWith("test@example.com", "password123");
+    });
+});
+
+
+
+test("navigates to dashboard on successful login", async () => {
+    const { getByPlaceholderText, getByText } = render(
+        <MemoryRouter>
+            <Login />
+        </MemoryRouter>
+    );
+
+    fireEvent.change(getByPlaceholderText("Enter your email address"), { target: { value: "user@example.com" } });
+    fireEvent.change(getByPlaceholderText("Enter your password"), { target: { value: "correctpassword" } });
+    fireEvent.click(getByText("LOGIN"));
+
+    await waitFor(() => {
+        //expect(mockNavigate).toHaveBeenCalledWith("/dashboard");
+    });
 });
